b3b925bbea9bf7c4995d321d1b5c03348e924244
KEPLERMH <keplermatosh@gmail.com>
2026-01-09 12:07:31 -0500

    Subiendo el hero al nivel visual

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        float: "float 2s ease-in-out infinite",
+      },
+      colors: {
+        maintext: "#FFFFF",
+        secondarytext: "#D1D5DB",
+        trirdtext: "#9CA3AF ",
+        acentocyan: "#67E8F9",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "transLateY(0)" },
+          "50%": { transform: "transLateY(8px)" },
+        },
+      },
+    }
+  },
+  plugins: [],
+}
+
+
