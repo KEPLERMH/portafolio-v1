3bc0ab0040a6a9b7922f95343834e25ad1ede170
KEPLERMH <keplermatosh@gmail.com>
2026-02-12 19:29:24 -0500

    Se agrego nuevo proyecto en la seccion proyectos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
 import p1 from "../assets/img/projects/weather-img.png"
-import p2 from "../assets/img/projects/p-2.jfif"
+import p2 from "../assets/img/projects/techStore-img.png"
 import p3 from "../assets/img/projects/p-3.jfif"
 
 export const projects = [
@@ -12,15 +12,15 @@ export const projects = [
         github: "https://github.com/KEPLERMH/Weather-App",
         demo: "https://weatherapp-pearl-five.vercel.app",
     },
-    // {
-    //     id: 2,
-    //     title: "AI Design Tool",
-    //     description: "Herramienta de diseño asistida por IA para crear interfaces de usuario automáticamente.",
-    //     image: p2,
-    //     techs: ["React", "Tailwind", "Javascript"],
-    //     github: "https://github.com/tuusuario/ecommerce",
-    //     demo: "https://ecommerce-demo.com",
-    // },
+    {
+        id: 2,
+        title: "Tech Store",
+        description: "Aplicación de carrito de compras desarrollada con React + TypeScript, utilizando Context API + useReducer.",
+        image: p2,
+        techs: ["React", "Tailwind", "Javascript", "TypeScript"],
+        github: "https://github.com/KEPLERMH/tech-store",
+        demo: "https://tech-store-lyart-three.vercel.app/",
+    },
     // {
     //     id: 3,
     //     title: "Social Media Dashboard",
